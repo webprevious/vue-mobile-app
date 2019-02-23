1ee6a8050fa6aba54a7d72e80a9043dffe277b28
bs <228713680@qq.com>
2019-02-23 12:59:38 +0800

    update online url

--- vue.config.js
+++ vue.config.js
@@ -19,7 +19,7 @@ module.exports = {
     proxy: {
       '^/api': {
         ws: false,
-        target: 'http://wzh1.run.hzmantu.com',
+        target: 'https://api.local.hzmantu.com',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
